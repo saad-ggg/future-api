dde14e6eb602ff4090e7455bda41deb6344249aa
saad-ggg <saad.a.e.selit@gmail.com>
2026-01-15 15:59:49 +0200

    Final Fix: Resolving Namespaces and cleaning routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProfileController;
-use App\Http\Controllers\Api\Admin\UserController;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |-------------------------------------------------------------------------- 
